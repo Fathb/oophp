1317afc69c5f2f7723f85d4b497b402448f9cfc3
Fathb <45653036+Fathb@users.noreply.github.com>
2020-06-16 15:36:29 +0700

    update Construc.php

--- Construc.php
+++ Construc.php
@@ -4,15 +4,16 @@ class turnament {
   public  $nama,
           $feeregist,
           $timeregist,
+          $TM,
           $matchday,
           $rule,
-          $winner,
           $payment;
   
-  public function __construct ($nama, $feeregist, $timeregist, $matchday, $rule, $payment= "OVO, DANA, GO-PAY, BRI"){
+  public function __construct ($nama = "nama", $feeregist = "harga", $timeregist = "tanggal", $TM = "tanggal", $matchday = "tanggal", $rule = "peraturan", $payment= "OVO, DANA, GO-PAY, BRI"){
     $this->nama=$nama;
     $this->feeregist=$feeregist;
     $this->timeregist=$timeregist;
+    $this->TM=$TM;
     $this->matchday=$matchday;
     $this->rule=$rule;
     $this->payment=$payment;
@@ -20,20 +21,24 @@ class turnament {
   
           
   public function getlabel (){
-    return "$this->nama, $this->feeregist, $this->timeregist, $this->timeregist,$this->rule, $this->payment";
+    return "$this->nama, $this->feeregist, $this->timeregist, $this->TM, $this->rule, $this->payment";
   }
 }
 
-$MCML = new turnament("MAGIC CHESS TOURNEY", 15000, "21-29 juni 2020", "1-3 juli 2020", "BO4 ke babak selanjutnya");
-      $ML->TM="30-6-2020";
+$MCML = new turnament("MAGIC CHESS TOURNEY", 15000, "21-29 juni 2020", "30-6-2020", "1-3 juli 2020", "BO4 ke babak selanjutnya");
       
-$ML = new turnament("MOBILE LEGENDS TOURNEY",40000,"21-29 juni 2020", "BO1, semifinala dan final BO3", "3-5 juli 2020");
-      $ML->TM="30-6-2020";
+$ML = new turnament("MOBILE LEGENDS TOURNEY",40000,"21-29 juni 2020", "30-60-2020", "BO1, semifinala dan final BO3", "3-5 juli 2020");
+$tour = new turnament("pes2020");
       
 echo "<hr>";
 echo $MCML->getlabel();
 echo "<br>";
 echo $ML->getlabel();
+echo "<hr>";
+echo $tour->nama ."<br>";
+echo $tour->feeregist ."<br>";
+echo $tour->timeregist ."<br>";
+echo $tour->TM ."<br>";
 
 
 ?>
