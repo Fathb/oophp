8bc8d031d246e308f995d924b73bab695dbc6d4e
Fathb <45653036+Fathb@users.noreply.github.com>
2020-06-20 23:38:57 +0700

    update Overriding.php

--- Overriding.php
+++ Overriding.php
@@ -1,1 +1,6 @@
-
+public $nick;
+  
+  public function __construct ($nama = "nama", $feeregist = "harga", $timeregist = "tanggal", $TM = "tanggal", $matchday = "tanggal", $rule = "peraturan", $nick, $payment= "OVO, DANA, GO-PAY, BRI"){
+    
+    parent::__construct($nama, $feeregist, $timeregist, $TM, $matchday, $rule, $nick, $payment);
+    $this->nick=$nick;
